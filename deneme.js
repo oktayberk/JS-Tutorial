3139ba0ccad2cd391f17a719f808eead900d60f8
Oktay Berk Altıntaş <65086193+oktayberk@users.noreply.github.com>
2022-11-24 02:00:59 +0300

    Add files via upload

---
+++ deneme.js
@@ -0,0 +1,5 @@
+var sehirler = ["ankra", "istanbul", "istal", "adana"];
+console.log(sehirler.pop())
+console.log(sehirler)
+console.log(sehirler.length)
+console.log(sehirler.concat(["ahmetmehmet"]))
