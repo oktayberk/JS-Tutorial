12c726f101f15a98230519b64e254c4ff89aa026
Oktay Berk Altıntaş <65086193+oktayberk@users.noreply.github.com>
2022-12-03 01:48:26 +0300

    Update ileriDizi.js

--- ileriDizi.js
+++ ileriDizi.js
@@ -6,18 +6,16 @@ dizi.forEach(sayi =>{
 console.log(kareDizisi);
 
 //map
-
 const mapDizi= dizi.map(sayi =>sayi * 3) //butun elemanları 3 ile çarp ve mapdizi ye aktar
 console.log(mapDizi);
 
 //filter
-
 const filtreliDizi = dizi.filter(sayi => sayi > 2)  //butun elemanları gez elemanlardan >2 olanları filtrelidizi içine aktar
 console.log(filtreliDizi);
 
 //reduce
-
 const toplam = dizi.reduce((acc, sayi) => {  //acc baslangıc degeri ve defaultu 0, burada butun sayıları topluyorz.
     return acc + sayi;
+    
 })
 console.log(toplam);
